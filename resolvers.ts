075ec96409b646ecf709d74619443f2ec7326acd
David Arévalo <72552800+davidarev@users.noreply.github.com>
2025-01-30 17:49:21 +0100

    Update resolvers.ts

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,7 @@
 export const resolvers = {
     Query: {
         test: async (): Promise<string> => {
-            const test = "esto es una prueba"
+            const test = "esto es una prueba JAJAJAJAJAJA"
             return test
         }
     }
