90d25e07119532cb6fd1ca62ffcee516ab23575f
David Arévalo <72552800+davidarev@users.noreply.github.com>
2025-01-31 17:50:04 +0100

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,5 +1,6 @@
 import {OptionalId} from "mongodb"
 
+// Objeto a almacenar en MongoDB
 export type RestauranteModel = OptionalId<{
     nombre: string,
     direccion: string,
@@ -10,20 +11,25 @@ export type RestauranteModel = OptionalId<{
     telefono: string,
 }>
 
+//APIs NINJA
+// https://api.api-ninjas.com/v1/validatephone
 export type APIPhone = {
     country: string
     is_valid: boolean
 }
 
+// https://api.api-ninjas.com/v1/city
 export type APICity = {
     latitude: number,
     longitude: number,
 }
 
+// https://api.api-ninjas.com/v1/weather
 export type APIWeather = {
     temp: number
 }
 
+// https://api.api-ninjas.com/v1/worldtime
 export type APIWorldTime = {
     hour: string,
     minute: string,
